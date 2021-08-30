cca0bc24f82020bcfbfb6833a0e797bb4fbd91d1
Patrick Pinto <patrick.cb3@gmail.com>
2021-08-30 10:18:20 +0100

    first commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,23 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
   end
+
+  def about
+  end
+
+  # def new
+  #   @page = Page.new
+  # end
+
+  # def create
+  #   @page = Page.new(page_params)
+  #   if @page.save
+  #     flash[:notice] = 'Page successfully created'
+  #     redirect_to :index, notice: 'Page successfully created'
+  #   else
+  #     render :new
+  #   end
+  # end
 end
